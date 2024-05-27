4e85e45c34fcb3786942a21b1becff6f012def79
M. LAEEQ KHAN <110678008+MLaeeqKhan@users.noreply.github.com>
2024-05-27 15:34:34 +0500

    Update auth.js

--- Router/auth.js
+++ Router/auth.js
@@ -4,7 +4,7 @@ var nodemailer = require('nodemailer');
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../Models/UserSchema");
 const generateToken = require("../Middleware/generateToken");
 router.use(express.json());
